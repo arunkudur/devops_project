97f663bf5ea147a39f7656e0505226b6fd0960cf
uig11271 <arun.g.v@continental-corporation.com>
2025-05-12 16:30:54 +0530

    Adding simple greeting message and testing it.

---
+++ src/greet.py
@@ -0,0 +1,6 @@
+def greet():
+    return "hello, welcome to devops world!"
+
+if __name__ == "__main__":
+    print(greet())
+    
---
+++ test/test_greet.py
@@ -0,0 +1,4 @@
+from src.greet import greet
+
+def test_greet():
+    assert greet() == "hello, welcome to devops world!"
